215ef76cb77385392fbe2d6671a38168a70158e7
ShotaSasaki-HU <invisiblefrontline@gmail.com>
2025-10-08 18:11:42 +0900

    feat: スターリンクトレインの可能性が高い打ち上げグループを特定するスクリプト．平均近点角の円周標準偏差と，打ち上げ年を基に特定する．

--- PoC/Skyfield/potential_train.py
+++ PoC/Skyfield/potential_train.py
@@ -1,7 +1,42 @@
-from skyfield.api import Topos, load
+from skyfield.api import load
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from datetime import datetime
+
+def build_name_to_instance_map(path_txt: str) -> dict:
+    """衛星名に対するインスタンスの辞書を作成"""
+    starlink_all_TLEs = load.tle(path_txt)
+
+    starlink_name_to_instance = {}
+    for name, satellite_instance in starlink_all_TLEs.items():
+        if 'STARLINK' in str(name):
+            starlink_name_to_instance[str(name)] = satellite_instance
+    
+    return starlink_name_to_instance
+
+def build_group_to_names_map(path_csv):
+    """打ち上げグループ名に対する衛星名の辞書を作成"""
+    df = pd.read_csv(path_csv, encoding='utf-8', header=0)
+    # 国際衛星識別番号から打ち上げグループ名を抽出
+    df['LAUNCH_GROUP'] = df['OBJECT_ID'].str.extract(r'(\d{4}-\d{3})')
+    # グループ名でグループ化
+    return df.groupby('LAUNCH_GROUP')['OBJECT_NAME'].apply(list).to_dict()
+
+def build_group_to_instances(name_to_instance: dict, group_to_names: dict) -> dict:
+    """打ち上げグループ名に対するインスタンス群の辞書を作成"""
+    launch_groups_with_instances = {} # {グループ名: [衛星インスタンスのリスト]}
+    for group, names in group_to_names.items():
+        instances = []
+        for name in names:
+            if name in name_to_instance:
+                instances.append(name_to_instance[name])
+            else:
+                raise ValueError(f"対応するインスタンスが見つかりません．: {name}")
+            
+        if instances:
+            launch_groups_with_instances[group] = instances
+    
+    return launch_groups_with_instances
 
 def calc_circular_std(rads: list) -> float:
     """
@@ -29,58 +64,38 @@ def calc_circular_std(rads: list) -> float:
     circular_std_rad = np.sqrt(-2 * np.log(r_bar))
     return circular_std_rad
 
-def find_potential_trains(launch_groups: dict, circular_std_threshold_rad: float) -> list:
-    pass
+def find_potential_trains(
+        group_to_instances: dict,
+        circular_std_threshold: float = 1.0,
+        max_age_days: int = 90 # 打ち上げからN日以内のグループのみを対象
+    ) -> dict:
+    """TLEからトレイン状態にある可能性が高いグループを特定する．"""
+    potential_trains = {}
 
-# 1. 衛星名に対するインスタンスの辞書を作成
-path_sup_gp_txt = "./PoC/Skyfield/sup-gp_starlink_20251008.txt"
-starlink_all_TLEs = load.tle(path_sup_gp_txt)
+    for group_name, instances in group_to_instances.items():
+        # 打ち上げ年フィルタ
+        launch_year = int(group_name.split('-')[0])
+        current_year = datetime.now().year
+        # 今年か去年のみが通過
+        if launch_year < (current_year - 1):
+            continue
 
-starlink_name_to_instance = {}
+        # グループ内の全衛星から平均近点角（ラジアン）を抽出
+        mean_anomalies_rad = [instance.model.mo for instance in instances]
+        circular_std = calc_circular_std(mean_anomalies_rad)
 
-for name, satellite_instance in starlink_all_TLEs.items():
-    if 'STARLINK' in str(name):
-        starlink_name_to_instance[str(name)] = satellite_instance
-
-# 2. 打ち上げグループ名に対する衛星名の辞書を作成
-path_sup_gp_csv = "./PoC/Skyfield/sup-gp_starlink_20251008.csv"
-# path_sup-gp_csv = "https://celestrak.org/NORAD/elements/supplemental/sup-gp.php?FILE=starlink&FORMAT=csv"
-df = pd.read_csv(path_sup_gp_csv, encoding='utf-8', header=0)
-df['INTERNATIONAL_DESIGNATOR'] = df['OBJECT_ID'].str.extract(r'(\d{4}-\d{3})')
-
-# 3. 打ち上げグループ名に対するインスタンス群の辞書を作成
-launch_groups = {} # {グループ名: [衛星インスタンスのリスト]}
-
-for launch_group in df['INTERNATIONAL_DESIGNATOR'].unique():
-    df_current_group = df[df['INTERNATIONAL_DESIGNATOR'] == launch_group]
-    object_names = df_current_group['OBJECT_NAME'].to_list()
-
-    instances = []
-
-    for name in object_names:
-        if name in starlink_name_to_instance:
-            instances.append(starlink_name_to_instance[name])
-        else:
-            raise ValueError(f"対応するインスタンスが見つかりません．: {name}")
+        if circular_std < circular_std_threshold:
+            potential_trains[group_name] = instances
     
-    launch_groups[launch_group] = instances
+    return potential_trains
 
-group_names = []
-circular_stds = []
+if __name__ == "__main__":
+    path_sup_gp_txt = "./PoC/Skyfield/sup-gp_starlink_20251008.txt"
+    path_sup_gp_csv = "./PoC/Skyfield/sup-gp_starlink_20251008.csv"
 
-print("打ち上げグループ: 平均近点角の円周標準偏差（ラジアン）")
-for group_name, instances in launch_groups.items():
-    # グループ内の全衛星から平均近点角（ラジアン）を抽出
-    mean_anomalies_rad = [instance.model.mo for instance in instances]
+    name_to_instance = build_name_to_instance_map(path_txt=path_sup_gp_txt)
+    group_to_names = build_group_to_names_map(path_csv=path_sup_gp_csv)
+    group_to_instances = build_group_to_instances(name_to_instance=name_to_instance, group_to_names=group_to_names)
 
-    group_names.append(group_name)
-    circular_stds.append(calc_circular_std(rads=mean_anomalies_rad))
-
-plt.figure(figsize=(10, 6))
-plt.plot(group_names, circular_stds, color='skyblue')
-plt.xlabel("Launch Group")
-plt.ylabel("Circular Standard Deviation [rad]")
-plt.title("Circular Std of Mean Anomalies by Launch Group")
-plt.xticks(rotation=45, ha='right')  # ラベルを見やすく回転
-plt.tight_layout()
-plt.show()
+    potential_trains = find_potential_trains(group_to_instances=group_to_instances)
+    print(f"潜在的なスターリンクトレイン: {list(potential_trains.keys())}")
