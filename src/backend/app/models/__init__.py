82c4b122e2bc2cd173dd793130097c836bbbeb5a
ShotaSasaki-HU <invisiblefrontline@gmail.com>
2025-10-13 02:12:07 +0900

    chore: 説明文の修正

--- src/backend/app/models/__init__.py
+++ src/backend/app/models/__init__.py
@@ -1,4 +1,5 @@
 # app/models/__init__.py
-# 複数のモデルをまとめてimportできるようにしておく．
-# main.pyなどで from app.models import Base のようにまとめて扱いやすくなる？
+# __init__.py に例えば from .municipality import Municipality と書くことで，app/models/municipality.pyファイルの中に定義されているMunicipalityクラスを，modelsパッケージの直下にあるかのように昇格させることができます．
+# このおかげでcrudなどにおいて，from app import models と書けば models.Municipality とテーブル定義を指定できる．
+# これが無いと，from app.models.municipality import Municipality と書かなければならない．
 from .municipality import Municipality
