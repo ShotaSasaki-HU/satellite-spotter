833ae18b72d5383f4510497661523501533d6c42
ShotaSasaki-HU <invisiblefrontline@gmail.com>
2025-10-13 20:57:13 +0900

    update: app/main.pyでspotsルーターを読み込み

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,9 +1,11 @@
+# app/main.py
 from fastapi import FastAPI
-from app.routers import locations
+from app.routers import locations, spots
 
 app = FastAPI()
 
 app.include_router(locations.router)
+app.include_router(spots.router)
 
 @app.get("/")
 def read_root():
